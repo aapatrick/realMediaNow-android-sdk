f61069373d481cc678d5acba09c66cb6284497ac
Patrick Abdul-Ahad <patrick.abdul-ahad@outlook.com>
2019-01-13 15:58:45 +0000

    created new class called PlayerConfig and added the API key to it (for playing a youtube video)

---
+++ sample/src/main/java/com/sparklit/sample/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.sparklit.sample;
+
+public class PlayerConfig {
+    PlayerConfig()
+    {
+
+    }
+    public static final String API_KEY ="AIzaSyByXNYtqTxq45ZdthUy7E3mTXjOBhGIUss";
+}
